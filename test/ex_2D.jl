963a36e071ed10e5c631b2bba26f8bf9800169ce
dehann <dehann.fourie@gmail.com>
2018-10-01 18:37:32 -0400

    more testing with 3 components on line

--- test/ex_2D.jl
+++ test/ex_2D.jl
@@ -3,14 +3,17 @@
 using ApproxManifoldProducts
 using Base: Test
 
-@testset "basic linear 2D find mu and Lambdas Gaussian product test..." begin
 
 Λ1 = 1.0
 Λ2 = 0.25
+Λ3 = 5/4.0
 
 μ1 = 1.0
 μ2 = 4.0
+μ3 = 2.4
 
+
+@testset "basic linear 2D find mu and Lambdas Gaussian product test..." begin
 
 Lambdas = [Λ1; Λ2]
 mus = [μ1; μ2]
@@ -41,6 +44,24 @@ end
 # u,s,v = svd(Lambdas*(Lambdas'))
 #
 # s[1,1]*u[:,2]
+
+@testset "test with 3 components on basic Euclidean line..."
+
+Lambdas = [Λ1; Λ2; Λ3]
+mus = [μ1; μ2; μ3]
+
+μ = get2DMu(mus, Lambdas)
+Λ = get2DLambda(Lambdas)
+
+@test abs(μ[1] - 2.0) < 1e-10
+
+@test abs(Λ - 10/4) < 1e-10
+
+
+
+
+end
+
 
 
 
