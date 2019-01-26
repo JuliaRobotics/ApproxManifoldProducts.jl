f979442f122f4c23f47f2d5ef575bb9c70cb6074
dehann <dehann.fourie@gmail.com>
2019-01-26 16:33:42 -0500

    more triple product examples working

--- examples/CircularProductExample.jl
+++ examples/CircularProductExample.jl
@@ -31,8 +31,9 @@ addtheta(wth1::Float64, wth2::Float64) = TU.wrapRad( wth1+wth2 )
 
 # manifold get Gaussian products mean
 
-getCircMu = (m::Vector{Float64}, s::Vector{Float64}, dummy::Float64) -> TU.wrapRad(get2DMuMin(m, s, diffop=difftheta, initrange=(-pi+0.0,pi+0.0)))
 
+getCircMu = (m::Vector{Float64}, s::Vector{Float64}, dummy::Float64) -> TU.wrapRad(get2DMu(m, s, diffop=difftheta, initrange=(-pi+0.0,pi+0.0)))
+# getCircMu = (m::Vector{Float64}, s::Vector{Float64}, dummy::Float64) -> TU.wrapRad(get2DMuMin(m, s, diffop=difftheta, initrange=(-pi+0.0,pi+0.0)))
 
 
 # create two densities
@@ -59,13 +60,11 @@ plotKDECircular([pc1;pc2])
 dummy = kde!(rand(100),[1.0;]);
 
 pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2], nothing, nothing, Niter=1, addop=addtheta, diffop=difftheta, getMu=getCircMu);
-pGM
-
 
 
 pc12 = kde!_CircularNaiveCV(pGM[:])
-getBW(pc12)[1,1]
-pc12 = kde!(pGM, [0.1;])
+# getBW(pc12)[1,1]
+# pc12 = kde!(pGM, [0.1;])
 
 pl = plotKDECircular([pc1;pc2; pc12])
 
@@ -82,7 +81,131 @@ pl |> PDF("/tmp/test.pdf", 20cm,15cm)
 
 
 
+
+## Different two
+
+
+
+pts1 = TU.wrapRad.(0.1*randn(100).-0.5)
+pts2 = TU.wrapRad.(0.1*randn(100).+0.5)
+
+# pc1 = kde!_CircularNaiveCV(pts1)
+# pc2 = kde!_CircularNaiveCV(pts2)
+pc1 = kde!(pts1, [0.1;])
+pc2 = kde!(pts2, [0.1;])
+
+
+# TODO: make circular KDE
+dummy = kde!(rand(100),[1.0;]);
+
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2], nothing, nothing, Niter=1, addop=addtheta, diffop=difftheta, getMu=getCircMu);
+
+
+pc12 = kde!_CircularNaiveCV(pGM[:])
+pl = plotKDECircular([pc1;pc2; pc12])
+
+
+
+
+
+## another two equally spaced
+
+
+
+
+
+pts1 = TU.wrapRad.(0.1*randn(100).-pi/2.0)
+pts2 = TU.wrapRad.(0.1*randn(100).+pi/2.0)
+
+# pc1 = kde!_CircularNaiveCV(pts1)
+# pc2 = kde!_CircularNaiveCV(pts2)
+pc1 = kde!(pts1, [0.1;])
+pc2 = kde!(pts2, [0.1;])
+
+
+# TODO: make circular KDE
+dummy = kde!(rand(100),[1.0;]);
+
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2], nothing, nothing, Niter=1, addop=addtheta, diffop=difftheta, getMu=getCircMu);
+
+
+pc12 = kde!_CircularNaiveCV(pGM[:])
+pl = plotKDECircular([pc1;pc2; pc12])
+
+
+
+
+
+
 ## handrolically trying product of 3
+
+
+
+
+pts1 = TU.wrapRad.(0.1*randn(100))
+pts2 = TU.wrapRad.(0.1*randn(100).-2pi/3.0.+1.0)
+pts3 = TU.wrapRad.(0.1*randn(100).+2pi/3.0)
+
+
+# pc1 = kde!_CircularNaiveCV(pts1)
+# pc2 = kde!_CircularNaiveCV(pts2)
+pc1 = kde!(pts1, [0.1;])
+pc2 = kde!(pts2, [0.1;])
+pc3 = kde!(pts3, [0.1;])
+
+
+pl = plotKDECircular([pc1;pc2;pc3])
+
+
+dummy = kde!(rand(100),[1.0;]);
+
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2; pc3], nothing, nothing, Niter=1, addop=addtheta, diffop=difftheta, getMu=getCircMu);
+
+
+pc123 = kde!_CircularNaiveCV(pGM[:])
+
+
+pl = plotKDECircular([pc1;pc2;pc3; pc123])
+
+
+
+
+## product of 3
+
+
+
+
+pts1 = TU.wrapRad.(0.1*randn(100))
+pts2 = TU.wrapRad.(0.1*randn(100).-2pi/3.0.+1.0)
+pts3 = TU.wrapRad.(0.1*randn(100).+2pi/3.0.-1.0)
+
+
+# pc1 = kde!_CircularNaiveCV(pts1)
+# pc2 = kde!_CircularNaiveCV(pts2)
+pc1 = kde!(pts1, [0.1;])
+pc2 = kde!(pts2, [0.1;])
+pc3 = kde!(pts3, [0.1;])
+
+
+pl = plotKDECircular([pc1;pc2;pc3])
+
+
+dummy = kde!(rand(100),[1.0;]);
+
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2; pc3], nothing, nothing, Niter=1, addop=addtheta, diffop=difftheta, getMu=getCircMu);
+
+
+pc123 = kde!_CircularNaiveCV(pGM[:])
+
+
+pl = plotKDECircular([pc1;pc2;pc3; pc123])
+
+
+
+
+
+
+## product of 3
 
 
 
@@ -94,7 +217,7 @@ pts3 = TU.wrapRad.(0.1*randn(100).+2pi/3.0)
 
 # pc1 = kde!_CircularNaiveCV(pts1)
 # pc2 = kde!_CircularNaiveCV(pts2)
-pc1 = kde!(pts1, [0.5;])
+pc1 = kde!(pts1, [0.1;])
 pc2 = kde!(pts2, [0.1;])
 pc3 = kde!(pts3, [0.1;])
 
@@ -104,16 +227,14 @@ pl = plotKDECircular([pc1;pc2;pc3])
 
 dummy = kde!(rand(100),[1.0;]);
 
-pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2], nothing, nothing, Niter=1, addop=addtheta, diffop=difftheta, getMu=getCircMu);
-pGM
-
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2; pc3], nothing, nothing, Niter=1, addop=addtheta, diffop=difftheta, getMu=getCircMu);
 
 
 pc123 = kde!_CircularNaiveCV(pGM[:])
 
 
-
 pl = plotKDECircular([pc1;pc2;pc3; pc123])
+
 
 
 
