9be32d52cd830a634e36d64a060b2f01ad2054e7
dehann <dehann.fourie@gmail.com>
2019-02-04 18:39:43 -0500

    some cleanup

--- test/basics.jl
+++ test/basics.jl
@@ -22,15 +22,15 @@ using KernelDensityEstimate
 
 
 # TODO ensure this manifold product is still valid
-@testset "basic tests..." begin
-
-
-mbe1 = ManifoldBelief(EuclideanManifold, 0.0)
-mbe2 = ManifoldBelief(EuclideanManifold, 0.0)
-
-*([mbe1;mbe2])
-
-end
+# @testset "basic tests..." begin
+#
+#
+# mbe1 = ManifoldBelief(EuclideanManifold, 0.0)
+# mbe2 = ManifoldBelief(EuclideanManifold, 0.0)
+#
+# *([mbe1;mbe2])
+#
+# end
 
 ##
 
