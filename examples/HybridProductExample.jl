0c1f5dc6e864c62eea515365f77e02f5eed7de89
dehann <dehann.fourie@gmail.com>
2019-02-05 18:05:20 -0500

    add hybrid manifold test

--- examples/HybridProductExample.jl
+++ examples/HybridProductExample.jl
@@ -13,7 +13,7 @@ using TransformUtils
 
 using RoMEPlotting
 
-# using Distributions
+using Distributions
 # using Random
 # using Optim
 # include(joinpath(dirname(@__FILE__), "circularEntropyUtils.jl"))
@@ -25,10 +25,12 @@ const AMP = ApproxManifoldProducts
 
 
 
+KDE.setForceEvalDirect!(true)
+
 ## create two densities
 
 
-pts1a = 150.0.+10.0*randn(100)
+pts1a = 180.0.+10.0*randn(100)
 pts2a = 200.0.+10.0*randn(100)
 
 pts1b = TU.wrapRad.(0.1*randn(100).-pi.+0.5)
@@ -39,13 +41,175 @@ pts2 = [pts2a';pts2b']
 
 # pc1 = kde!_CircularNaiveCV(pts1)
 # pc2 = kde!_CircularNaiveCV(pts2)
-pc1 = kde!(pts1, [0.1;1.0], (AMP.addtheta,), (AMP.difftheta,))
-pc2 = kde!(pts2, [0.1;1.0], (AMP.addtheta,), (AMP.difftheta,))
+pc1 = kde!(pts1, [3.0; 0.1], (+,AMP.addtheta), (-,AMP.difftheta))
+pc2 = kde!(pts2, [3.0; 0.1], (+,AMP.addtheta), (-,AMP.difftheta))
 
 
 
 
-# testing Euclidean dimension
+
+
+
+dummy = kde!(rand(2,100),[1.0;], (+,AMP.addtheta), (-,AMP.difftheta));
+
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2], nothing, nothing, Niter=1,
+                          addop=(+,AMP.addtheta), diffop=(-,AMP.difftheta), getMu=(KDE.getEuclidMu,AMP.getCircMu));
+#
+
+
+
+lin1 = getBW(kde!(pGM[1,:]))[:,1][1]
+cir1 = getBW(kde!_CircularNaiveCV(pGM[2,:]))[:,1][1]
+
+
+pc12 = kde!(pGM, [lin1;cir1], (+,AMP.addtheta), (-,AMP.difftheta))
+
+plotKDE([pc1;pc2; pc12], dims=[1],c=["red";"green";"magenta"])
+
+
+
+
+pl = plotKDECircular([marginal(pc1,[2]);marginal(pc2,[2]); marginal(pc12,[2])])
+
+
+
+pl = Gadfly.plot(y=pGM[1,:],x=pGM[2,:], Geom.histogram2d(xbincount=50,ybincount=30))
+
+
+pl |> PDF("/tmp/test.pdf",20cm,15cm)
+@async run(`evince /tmp/test.pdf`)
+
+
+
+
+##
+
+
+
+
+
+R_ = TU.R(10*pi/180.0)
+
+pts1 = rand(MvNormal([100.0;0.0], R_*[40.0 0.0; 0.0 0.2]*(R_')), 100)
+pts1[2,:] = TU.wrapRad.(pts1[2,:])
+# plot(x=pts1[1,:],y=pts1[2,:], Geom.histogram2d(xbincount=50,ybincount=30))
+
+
+R_ = TU.R(-10*pi/180.0)
+
+pts2 = rand(MvNormal([100.0;0.0], R_*[40.0 0.0; 0.0 0.2]*(R_')), 100)
+pts2[2,:] = TU.wrapRad.(pts2[2,:])
+plot(x=pts2[1,:],y=pts2[2,:], Geom.histogram2d(xbincount=50,ybincount=30))
+
+
+
+lin1 = getBW(kde!(pts1[1,:]))[:,1][1]
+cir1 = getBW(kde!_CircularNaiveCV(pts1[2,:]))[:,1][1]
+
+
+
+pc1 = kde!(pts1, [lin1; cir1], (+,AMP.addtheta), (-,AMP.difftheta))
+pc2 = kde!(pts2, [lin1; cir1], (+,AMP.addtheta), (-,AMP.difftheta))
+
+
+
+
+
+dummy = kde!(rand(2,100),[1.0;], (+,AMP.addtheta), (-,AMP.difftheta));
+
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2], nothing, nothing, Niter=1,
+                          addop=(+,AMP.addtheta), diffop=(-,AMP.difftheta), getMu=(KDE.getEuclidMu,AMP.getCircMu));
+#
+
+
+
+
+
+lin1 = getBW(kde!(pGM[1,:]))[:,1][1]
+cir1 = getBW(kde!_CircularNaiveCV(pGM[2,:]))[:,1][1]
+
+
+pc12 = kde!(pGM, [lin1;cir1], (+,AMP.addtheta), (-,AMP.difftheta))
+
+plotKDE([pc1;pc2; pc12], dims=[1],c=["red";"green";"magenta"])
+
+
+
+
+pl = plotKDECircular([marginal(pc1,[2]);marginal(pc2,[2]); marginal(pc12,[2])])
+
+
+
+
+
+pl = Gadfly.plot(y=pGM[1,:],x=pGM[2,:], Geom.histogram2d(xbincount=50,ybincount=30))
+
+
+
+##  Do crazier single and multiple
+
+
+
+
+
+R_ = TU.R(20*pi/180.0)
+
+pts1 = rand(MvNormal([100.0;0.0], R_*[30.0 0.0; 0.0 0.2]*(R_')), 100)
+pts1[2,:] = TU.wrapRad.(pts1[2,:])
+# plot(x=pts1[1,:],y=pts1[2,:], Geom.histogram2d(xbincount=50,ybincount=30))
+
+
+
+pts2a = [5.0*randn(50).+85; 5.0*randn(50).+115]
+pts2b = [TU.wrapRad.(0.1*randn(50).+2.6); TU.wrapRad.(0.1*randn(50).-2.6)]
+pts2 = [pts2a';pts2b']
+
+pts2 = pts2[:,randperm(100)]
+
+# plot(x=pts2[1,:],y=pts2[2,:], Geom.histogram2d(xbincount=50,ybincount=30))
+
+
+lin1 = getBW(kde!(pts1[1,:]))[:,1][1]
+cir1 = getBW(kde!_CircularNaiveCV(pts1[2,:]))[:,1][1]
+pc1 = kde!(pts1, [lin1; cir1], (+,AMP.addtheta), (-,AMP.difftheta))
+
+lin2 = getBW(kde!(pts2[1,:]))[:,1][1]
+cir2 = getBW(kde!_CircularNaiveCV(pts2[2,:]))[:,1][1]
+pc2 = kde!(pts2, [lin2; cir2], (+,AMP.addtheta), (-,AMP.difftheta))
+
+
+
+
+
+dummy = kde!(rand(2,100),[1.0;], (+,AMP.addtheta), (-,AMP.difftheta));
+
+pGM, = prodAppxMSGibbsS(dummy, [pc1; pc2], nothing, nothing, Niter=1,
+                          addop=(+,AMP.addtheta), diffop=(-,AMP.difftheta), getMu=(KDE.getEuclidMu,AMP.getCircMu));
+#
+
+
+
+
+
+lin1 = getBW(kde!(pGM[1,:]))[:,1][1]
+cir1 = getBW(kde!_CircularNaiveCV(pGM[2,:]))[:,1][1]
+
+
+pc12 = kde!(pGM, [lin1;cir1], (+,AMP.addtheta), (-,AMP.difftheta))
+
+
+pl = Gadfly.plot(x=pGM[1,:],y=pGM[2,:], Geom.histogram2d(xbincount=50,ybincount=30))
+
+
+# plotKDE([pc1;pc2; pc12], dims=[1],c=["red";"green";"magenta"])
+
+
+
+
+
+## testing Euclidean dimension separately
+
+
 
 pc1a = kde!(pts1a)
 pc2a = kde!(pts2a)
@@ -83,6 +247,23 @@ pl = plotKDECircular([marginal(pc1,[2;]);marginal(pc2,[2;]); marginal(pc12,[2])]
 
 
 Gadfly.plot((x)->marginal(pc12,[2])([x;])[1], -pi, pi)
+
+
+
+
+
+
+
+
+
+##
+
+
+
+pl |> PDF("/tmp/test.pdf",20cm,15cm)
+@async run(`evince /tmp/test.pdf`)
+
+
 
 
 
