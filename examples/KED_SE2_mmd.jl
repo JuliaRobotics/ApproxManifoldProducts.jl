8a43a9cb02457d83925094110ccc083afa4caf07
dehann <dehann.fourie@gmail.com>
2020-05-25 17:11:04 -0400

    udpate for new plotting

--- examples/KED_SE2_mmd.jl
+++ examples/KED_SE2_mmd.jl
@@ -4,16 +4,17 @@ using ApproxManifoldProducts
 # using JLD2
 # @load "mmdse2.jld2" pred meas
 
+# x,y,theta
 pred = randn(3,100)
 meas = randn(3,100)
 
 val = [0.0]
 mmd!(val, pred, meas, SE2_Manifold)
 
-
+val
 
 ## Can visualize two poses with
 
-using RoMEPlotting
+using RoMEPlotting, RoME
 
-plotFactorValues(Pose2Pose2, meas, pred)
+plotFactorValues(meas, pred, Pose2Pose2)
