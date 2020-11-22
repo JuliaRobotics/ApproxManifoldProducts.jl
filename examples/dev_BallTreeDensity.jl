5fa259038d9971fc45ba319103ab35ffcf11409a
dehann <dehann.fourie@gmail.com>
2020-11-22 14:13:34 -0500

    figuring out a route to MetricTrees

---
+++ examples/dev_BallTreeDensity.jl
@@ -0,0 +1,171 @@
+# build BallTree for KDE density estimation
+
+
+using NearestNeighbors: BallTree, RMSDeviation
+using Colors
+using PyPlot
+using PyCall
+@pyimport matplotlib.patches as patch
+
+using StaticArrays
+using LinearAlgebra
+
+using BenchmarkTools
+
+
+##
+
+allpts = [SVector(randn(2)...) for i in 1:100];
+
+tree = BallTree(allpts, RMSDeviation(); leafsize = 50)
+
+tree = BallTree(allpts, RMSDeviation(); leafsize = 25)
+
+tree = BallTree(allpts, RMSDeviation(); leafsize = 13)
+
+tree = BallTree(allpts, RMSDeviation(); leafsize = 7)
+
+tree = BallTree(rand(2,100), RMSDeviation(); leafsize = 1)
+
+
+
+## Make own distance
+
+# using TransformUtils
+using Manifolds
+
+# carfully only include a single definition for use with NearestNeighbors.jl
+using Distances: Metric
+
+
+struct ManiRMSDeviation{M <: Manifold} <: Metric where M
+  manifold::M
+end
+
+##
+
+
+
+mr = ManiRMSDeviation(Euclidean(2))
+
+
+
+##
+
+
+
+distance(mr.manifold, 1,2)
+
+
+##
+
+distance(mr.manifold, SVector(1,2),SVector(2,3))
+
+
+
+
+
+##
+
+
+M_se2 = SpecialEuclidean(2)
+
+
+sI = SMatrix{3,3,Float64}(diagm(ones(3)))
+
+T1 = Manifolds.hat(M_se2, sI, SVector(0,0,pi/2))
+
+T2 = Manifolds.hat(M_se2, sI, SVector(1,0,0))
+
+
+distance(M_se2, T1, T2)
+
+
+
+
+##
+
+
+md = ManiRMSDeviation{}()
+
+rd = RMSDeviation()
+
+isconcretetype(rd)
+
+# (dist::MeanSqDeviation)(a, b) = sqeuclidean(a, b) / length(a)
+# msd(a, b) = MeanSqDeviation()(a, b)
+
+# (dist::RMSDeviation)(a, b) = sqrt(MeanSqDeviation()(a, b))
+# rmsd(a, b) = RMSDeviation()(a, b)
+
+
+@code_warntype rmsd(1,1)
+
+
+
+sqeuclidean(a, b) / length(a)
+
+(::ManiRMSDeviation{T})(a, b) where T = sqrt(MeanSqDeviation()(a, b))
+
+
+
+
+##
+
+import NearestNeighbors.HyperSphere
+
+##
+
+# Adds a sphere to an axis
+function add_sphere(ax, hs::HyperSphere, col)
+    ell = patch.Circle(hs.center, radius = hs.r, facecolor="none", edgecolor=col)
+    ax.add_artist(ell)
+end
+
+# Skip non leaf nodes
+offset = tree.tree_data.n_internal_nodes + 1
+nleafs = tree.tree_data.n_leafs
+
+# Range of leaf nodes
+index_range = offset: offset + nleafs - 1
+
+# Generate some nice colors
+cols = distinguishable_colors(length(index_range), RGB(0,0,0))
+
+# Create figure
+cfig = figure()
+ax = cfig.add_subplot(1,1,1)
+ax.set_aspect("equal")
+axis((-.25,1.25,-.25,1.25))
+
+
+for (i, idx) = enumerate(index_range)
+  col = cols[i]
+  # Get the indices of the leaf nodes into the tree data
+  range = NearestNeighbors.get_leaf_range(tree.tree_data, idx)
+  d = tree.data[range]
+  
+  # Plot the points in the hyper spehre
+  plot(getindex.(d, 1), getindex.(d, 2), "*", color = (col.r, col.g, col.b))
+  
+  # And the hypersphere itself
+  sphere = tree.hyper_spheres[idx]
+  add_sphere(ax, sphere, (col.r, col.g, col.b))
+end
+
+title("Leaf nodes with their corresponding points")
+
+
+
+
+##
+
+using Distances
+
+
+MeanSqDeviation()([1;2;3;4], [2;3;4;5])
+
+
+
+
+#
