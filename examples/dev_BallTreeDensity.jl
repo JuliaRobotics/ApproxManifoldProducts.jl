aeeea52d45dbbf6304a28883cf83639708e4a56e
dehann <dehann.fourie@gmail.com>
2020-11-29 21:05:54 -0500

    wip on FakeArray

--- examples/dev_BallTreeDensity.jl
+++ examples/dev_BallTreeDensity.jl
@@ -38,14 +38,62 @@ idxs, dists = knn(balltree, coords(pts[2]), k)
 
 
 
+## try make ManifoldFakeArray
+
+mutable struct ManifoldFakeArray{T,N} <: Base.AbstractArray{T,N}
+    element::T
+end
+
+ManifoldFakeArray{N}(el::T) where {T,N} = ManifoldFakeArray{T,N}(el)
+
+
+struct ManifoldFakeArrayDistance{TM<:Manifold} <: Distances.Metric
+    manifold::TM
+end
+
+function (dist::ManifoldFakeArrayDistance{TM})(a, b) where TM
+    return distance(dist.manifold, a.element, b.element)
+end
 
 
 
+import Base: size
+Base.size(el::Union{<:ManifoldFakeArray{T,N},Type{<:ManifoldFakeArray{T,N}}}) where {T,N} = N
+Base.length(el::Union{<:ManifoldFakeArray{T,N},Type{<:ManifoldFakeArray{T,N}}}) where {T,N} = N
+
+# Base.tail(el::ManifoldFakeArray{T,N}) where {T,N} = T[]
+
+function Base.show(io, mime, mdfa::ManifoldFakeArray{T,N}) where {T,N}
+    println(io, "ManifoldFakeArray{T,N}")
+    println(io, "  N: $N")
+    # println(io, "  T: $T")
+    # println(io, "    .element::T: $(mdfa.element)")
+end
+
+function Base.show(io, mime, mdfa::AbstractArray{ <: ManifoldFakeArray{T,N}}) where {T,N}
+    for i in 1:length(mdfa)
+        show(io,mime, mdfa[i])
+    end
+end
+
+Base.Multimedia.display(mdfa::ManifoldFakeArray{T,N}) where {T,N} = display(T)
+Base.Multimedia.display(mdfa::AbstractArray{<:ManifoldFakeArray{T,N}}) where {T,N} = display.(mdfa)
 
 
 
+faPts = map(x->ManifoldFakeArray{3}(x), pts)
 
 
+distFA = ManifoldFakeArrayDistance(M)
+
+
+distFA(faPts[1], faPts[2])
+
+
+
+## real test
+
+balltree = BallTree(faPts, distFA)
 
 
 ## build BallTree for KDE density estimation
