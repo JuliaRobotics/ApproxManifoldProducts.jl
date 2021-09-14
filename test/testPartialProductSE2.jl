c93948d7a3bf3027654d32ad436c8e260ffc5fdf
dehann <dehann.fourie@gmail.com>
2021-09-14 09:46:45 -0400

    wip

--- test/testPartialProductSE2.jl
+++ test/testPartialProductSE2.jl
@@ -21,22 +21,32 @@ pts2 = dict["pts2"]
 M = SpecialEuclidean(2)
 e0 = identity_element(M)
 
-# # p1 full SpecialEuclidean(2)
-# pts1 = [exp(M, e0, hat(M, e0, [randn(2); 0.1*randn()])) for _ in 1:100]
+# p1 full SpecialEuclidean(2)
 p1 = manikde!(M, pts1)
 p1_ = marginal(p1,[1;2])
 
-# # p2 only Translation(2) part
-# pts2 = vcat([exp(M, e0, hat(M, e0, [5;0;pi/4] .+ [5*randn(2);0.1*randn()])) for _ in 1:50], [exp(M, e0, hat(M, e0, [5;-5;pi/4] .+ [10*randn(2);0.1*randn()])) for _ in 1:50])
-# shuffle!(pts2)
+# p2 only Translation(2) part
 _p2 = manikde!(M, pts2)
 p2 = marginal(_p2, [1;2])
 
 # product of full and marginal
-p12 = p1*p2
+# p12 = p1*p2
+
+##
+
+selectedLabels=Vector{Vector{Int}}()
+p12 = manifoldProduct([p1; p2]; addEntropy=false,
+                                recordLabels=true,
+                                selectedLabels=selectedLabels  )
+##
 
 p12_ = marginal(p12, [1;2])
 
+## intermediate test, check product of selected kernels match what is in the marginal
+
+
+
+##
 
 # now do submanifold dimensions separately as reference test -- should get a similar result
 pts1_ = getPoints(p1_)
@@ -45,7 +55,14 @@ pts2_ = getPoints(p2)
 p1__ = manikde!(TranslationGroup(2), pts1_)
 p2__ = manikde!(TranslationGroup(2), pts2_)
 
-p12__ = p1__*p2__
+# p12__ = p1__*p2__
+selectedLabels__=Vector{Vector{Int}}()
+p12__ = manifoldProduct([p1__; p2__]; addEntropy=true,
+                                      recordLabels=true,
+                                      selectedLabels=selectedLabels__  )
+#
+
+##
 
 _p12_ = manikde!(TranslationGroup(2), getPoints(p12_))
 
@@ -57,7 +74,8 @@ _p12_ = manikde!(TranslationGroup(2), getPoints(p12_))
 # using Cairo, RoMEPlotting
 # Gadfly.set_default_plot_size(35cm,20cm)
 
-# plotKDE([p1_;p2; p12], levels=5)
+# plotKDE([p1_;p2; p12], levels=1, selectedPoints=selectedLabels[1:2])
+# n=3; plotKDE([p1__;p2__; p12__], levels=1, selectedPoints=selectedLabels__[n:n])
 # plotKDE([p1__; p2__; p12__])
 # plotKDE([_p12_; p12__])
 ##
