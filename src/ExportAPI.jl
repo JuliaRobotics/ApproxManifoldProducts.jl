2943e6eeb6c8393187b06e52f8bcc5139b55a603
dehann <dehann.fourie@gmail.com>
2022-07-14 17:15:01 -0700

    overload statistics mean var std cov

---
+++ src/ExportAPI.jl
@@ -0,0 +1,32 @@
+
+export  
+  # new local features
+  AMP,
+  MKD,
+  AbstractManifold,
+  ManifoldKernelDensity,
+  get2DLambda,
+  get2DMu,
+  get2DMuMin,
+  resid2DLinear,
+  solveresid2DLinear!,
+  solveresid2DLinear,
+  *,
+  isapprox,
+
+  # APi and util functions
+  buildHybridManifoldCallbacks,
+  getKDEManifoldBandwidths,
+  manifoldProduct,
+  manikde!,
+  calcCovarianceBasic,
+  isPartial,
+  calcProductGaussians
+
+export getPoints, getBW, Ndim, Npts
+export getKDERange, getKDEMax, getKDEMean, getKDEfit
+export sample, rand, resample, kld, minkld
+export calcMean
+export mean, cov, std, var
+export getInfoPerCoord, getBandwidth
+export antimarginal
