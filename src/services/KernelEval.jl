80d35d18d2d1e19de64368b44c77df5557f42356
Johannes Terblanche <Affie@users.noreply.github.com>
2024-04-29 17:42:20 +0200

    More testing on tree eval wip for discussion

--- src/services/KernelEval.jl
+++ src/services/KernelEval.jl
@@ -4,6 +4,7 @@ function projectSymPosDef(c::AbstractMatrix)
   s = size(c)
   # pretty fast to make or remake isbitstype form matrix
   _c = SMatrix{s...}(c)
+  #TODO likely not intended project here: see AMP#283
   issymmetric(_c) ? _c : project(SymmetricPositiveDefinite(s[1]),_c,_c)
 end
 
@@ -11,7 +12,7 @@ end
 Base.eltype(mt::MvNormalKernel) = eltype(mt.p)
 
 function MvNormalKernel(
-  μ::AbstractVector,
+  μ::AbstractArray,
   Σ::AbstractArray,
   weight::Real=1.0
 )
@@ -60,7 +61,7 @@ function Base.show(io::IO, mvk::MvNormalKernel)
   μ = mean(mvk)
   Σ2 = cov(mvk)
   # Σ=sqrt(Σ2)
-  d = length(μ)
+  d = size(Σ2,1)
   print(io, "MvNormalKernel(d=",d)
   print(io,",μ=",round.(μ;digits=3))
   print(io,",Σ^2=[",round(Σ2[1];digits=3))
