c3c1f0e509543841bb7bf7df360191f12e2194dd
dehann <dehann.fourie@gmail.com>
2024-07-15 04:59:57 -0700

    refac mani overloads, more adjs

--- src/ApproxManifoldProducts.jl
+++ src/ApproxManifoldProducts.jl
@@ -1,39 +1,44 @@
 module ApproxManifoldProducts
 
-using Reexport
-@reexport using KernelDensityEstimate
+import Base: *, isapprox, convert, show, eltype, length
+
+using Logging
+using StaticArrays
+using LinearAlgebra
+import LinearAlgebra: rotate!, det
+
+using TensorCast
+using DocStringExtensions
+using Distributions
 using Random
+import Random: rand
 
-import TransformUtils as TUs
-import TransformUtils: skew
+using Statistics
+import Statistics: mean, std, cov, var, entropy
+
 import Rotations as _Rot
+using CoordinateTransformations
 
-import ManifoldsBase
-import ManifoldsBase: AbstractManifold, distance
 using RecursiveArrayTools: ArrayPartition
 export ArrayPartition
 
+import ManifoldsBase
+import ManifoldsBase: AbstractManifold, distance
 using Manifolds
-
-using DocStringExtensions
 
 using NLsolve
 import Optim
-using CoordinateTransformations
-using Requires
-using LinearAlgebra
-using TensorCast
-using StaticArrays
-using Logging
-using Statistics
-using Distributions
 
-import Random: rand
+import TransformUtils as TUs
+import TransformUtils: skew
 
-import Base: *, isapprox, convert, show, eltype, length
-import LinearAlgebra: rotate!, det
-import Statistics: mean, std, cov, var, entropy
+using Reexport
+@reexport using KernelDensityEstimate
 import KernelDensityEstimate: getPoints, getBW, evalAvgLogL, entropy, evaluate
+
+
+# FIXME ON FIRE OBSOLETE REMOVE
+using Requires
 
 const MB = ManifoldsBase
 const CTs = CoordinateTransformations
