3f304e49aa6a7512c5ad33653efd103521e6d440
Johannes Terblanche <Affie@users.noreply.github.com>
2025-10-14 13:45:06 +0200

    Fixes for Manifolds v0.11

--- test/testManifoldConventions.jl
+++ test/testManifoldConventions.jl
@@ -1,32 +1,35 @@
 # test manifold conventions
 
 using Test
-import Manifolds as MF
-import LieGroups as LGr
+using LieGroups
+using RecursiveArrayTools
 import Rotations as _Rot
 
 ##
 @testset "test MF.jl conventions" begin
     ##
-
-    M = MF.SpecialEuclidean(2; vectors = MF.HybridTangentRepresentation())
-    e0 = MF.identity_element(M)
+    # TODO why this test?
+    # This test used to test deprecated SpecialEuclidean(2; vectors = HybridTangentRepresentation())
+    # this is the equivelent to make it work in LieGroups.jl
+    M = TranslationGroup(2) × SpecialOrthogonalGroup(2)
+    G = SpecialEuclideanGroup(2; variant = :right)
+    e0 = identity_element(M, ArrayPartition)
 
     # body to body next
     bTb_ = ArrayPartition([10.0; 0], _Rot.RotMatrix(pi / 2))
 
     # drive in a clockwise square from the origin
     wTb0 = ArrayPartition([100.0; 0], _Rot.RotMatrix(0.0))
-    wTb1 = MF.compose(M, wTb0, bTb_)    # right
-    wTb2 = MF.compose(M, wTb1, bTb_)    # top right
-    wTb3 = MF.compose(M, wTb2, bTb_)    # top
-    wTb4 = MF.compose(M, wTb3, bTb_)    # origin
+    wTb1 = compose(G, wTb0, bTb_)    # right
+    wTb2 = compose(G, wTb1, bTb_)    # top right
+    wTb3 = compose(G, wTb2, bTb_)    # top
+    wTb4 = compose(G, wTb3, bTb_)    # origin
 
-    wCb0 = MF.vee(M, e0, MF.log(M, e0, wTb0))
-    wCb1 = MF.vee(M, e0, MF.log(M, e0, wTb1))
-    wCb2 = MF.vee(M, e0, MF.log(M, e0, wTb2))
-    wCb3 = MF.vee(M, e0, MF.log(M, e0, wTb3))
-    wCb4 = MF.vee(M, e0, MF.log(M, e0, wTb4))
+    wCb0 = vee(M, e0, log(M, e0, wTb0))
+    wCb1 = vee(M, e0, log(M, e0, wTb1))
+    wCb2 = vee(M, e0, log(M, e0, wTb2))
+    wCb3 = vee(M, e0, log(M, e0, wTb3))
+    wCb4 = vee(M, e0, log(M, e0, wTb4))
 
     ##
 
@@ -42,16 +45,16 @@ import Rotations as _Rot
 
     # Use opposite convention from above to show it is wrong
     wTb0 = ArrayPartition([100.0; 0], _Rot.RotMatrix(0.0))
-    wTb1 = MF.compose(M, bTb_, wTb0)    # right
-    wTb2 = MF.compose(M, bTb_, wTb1)    # top right
-    wTb3 = MF.compose(M, bTb_, wTb2)    # top
-    wTb4 = MF.compose(M, bTb_, wTb3)    # origin
+    wTb1 = compose(G, bTb_, wTb0)    # right
+    wTb2 = compose(G, bTb_, wTb1)    # top right
+    wTb3 = compose(G, bTb_, wTb2)    # top
+    wTb4 = compose(G, bTb_, wTb3)    # origin
 
-    wCb0 = MF.vee(M, e0, MF.log(M, e0, wTb0))
-    wCb1 = MF.vee(M, e0, MF.log(M, e0, wTb1))
-    wCb2 = MF.vee(M, e0, MF.log(M, e0, wTb2))
-    wCb3 = MF.vee(M, e0, MF.log(M, e0, wTb3))
-    wCb4 = MF.vee(M, e0, MF.log(M, e0, wTb4))
+    wCb0 = vee(M, e0, log(M, e0, wTb0))
+    wCb1 = vee(M, e0, log(M, e0, wTb1))
+    wCb2 = vee(M, e0, log(M, e0, wTb2))
+    wCb3 = vee(M, e0, log(M, e0, wTb3))
+    wCb4 = vee(M, e0, log(M, e0, wTb4))
 
     # check the negative result
     @test isapprox([100, 0.0, 0], wCb0; atol = 1e-6)
