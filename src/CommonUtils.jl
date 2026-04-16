0b62fda3804dfcd5a1e625f7a4f997d79888bb67
dehann <dehann.fourie@gmail.com>
2026-04-15 23:36:44 -0700

    drop maniProd legacy, also depr other code

--- src/CommonUtils.jl
+++ src/CommonUtils.jl
@@ -1,49 +1,6 @@
 # Common Utils
 
 
-# """
-#     $SIGNATURES
-
-# A clunky repeat calculation of one product kernel.
-# """
-# function updateProductSample(
-#     dest::BallTreeDensity,
-#     proposals::Vector{BallTreeDensity},
-#     manifolds::Tuple,
-#     smplIdx::Int,
-#     labels::Vector{Int},
-# )
-#     #
-
-#     Ndens = length(proposals)
-#     Ndim = Ndim(dest)
-
-#     densLen = Npts.(proposals)
-
-#     calclambdas = zeros(Ndim, Ndens)
-#     calcmu = zeros(Ndim, Ndens)
-#     destMu = zeros(Ndim)
-#     destCov = 0.0
-
-#     @inbounds @fastmath @simd for dim = 1:Ndim
-#         for j = 1:Ndens
-#             calclambdas[dim, j] = 1.0 / (getBW(proposals[j]) .^ 2)[dim, labels[j]]
-#             calcmu[dim, j] = getPoints(proposals[j])[dim, labels[j]]
-#         end
-#         destCov = getLambda(calclambdas)
-#         destCov = 1.0 / destCov
-#         # μ = 1/Λ * Λμ  ## i.e. already scaled to mean only
-#         destMu[dim] = getMu(calcmu[dim, :], calclambdas[dim, :], destCov)
-#     end
-
-#     # previous points
-#     pts = getPoints(dest)
-#     pts[:, smplIdx] = destMu
-
-#     return manikde!(pts, manifolds)
-# end
-
-
 
 function calcProductGaussians_flat(
     M::AbstractManifold,
