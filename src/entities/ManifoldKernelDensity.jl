3004bf34c1881ba56a73c1bc61307a395c612562
dehann <dehann.fourie@gmail.com>
2026-04-18 14:52:42 -0700

    drop ManellicTree type

--- src/entities/ManifoldKernelDensity.jl
+++ src/entities/ManifoldKernelDensity.jl
@@ -47,7 +47,8 @@ HomotopyDensity{
   leaf_kernels::SizedVector{N,HL},
   tree_kernels::SizedVector{N,HT},
   kw...
-) where {M, L, D, N, HL, HT} = HomotopyDensity{L, M, D, length(data), HL, HT}(;
+) where {L, M, D, N, HL, HT} = 
+HomotopyDensity{L, M, D, length(data), HL, HT}(;
   manifold,
   data,
   leaf_kernels,
@@ -77,51 +78,5 @@ struct ManifoldKernelDensity{M <: MB.AbstractManifold, B <: HomotopyDensity, L, 
     _u0::P
     infoPerCoord::Vector{Float64}
 end
-# const MKD{M, B, L} = ManifoldKernelDensity{M, B, L}
 
 
-
-
-## LEGACY BELOW
-
-
-# """
-#     $TYPEDEF
-
-# Elliptical structure for use in a (Manellic) Ball Tree.
-# """
-# struct HyperEllipse{P <:AbstractArray,D,DD}
-#   """ manifold point at which this ellipse is based """
-#   point::P
-#   """ Covariance of coords at either TBD this point or some other reference point? """
-#   coord_cov::SMatrix{D,D,Float64,DD}
-# end
-
-# ManellicTree
-
-# Short for Manifold Ellipse Metric Tree
-# starting as a balanced tree, relax to unbalanced in future.
-struct ManellicTree{
-  M, 
-  D <: AbstractVector, 
-  N, 
-  HL, 
-  HT
-}
-    manifold::M
-    data::D
-    weights::MVector{N, <:Real}  # TODO rename to mixture_weights
-    # TODO introduce weights
-    permute::MVector{N, Int}
-    # kernels::ArrayPartition{<:Number,KT}
-    """ These leaf kernels store a duplication of .data[.permute] """
-    leaf_kernels::SizedVector{N, HL}  # TODO rename to twig_kernels
-    tree_kernels::SizedVector{N, HT}  # TODO rename to mixture_kernels
-    segments::SizedVector{N, Set{Int}}
-    # left_idx::MVector{N,Int}
-    # right_idx::MVector{N,Int}
-
-    # workaround to overcome bug for StaticArrays `isdefined() != false` issue
-    _workaround_isdef_leafkernel::Set{Int}
-    _workaround_isdef_treekernel::Set{Int}
-end
