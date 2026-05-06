30db61d8aaad5ad78eb39f033fcbef78311a034f
dehann <dehann.fourie@gmail.com>
2026-05-06 02:31:25 -0700

    wip

--- test/devHomotopyDensitySerde.jl
+++ test/devHomotopyDensitySerde.jl
@@ -4,7 +4,13 @@ using Test
 using ApproxManifoldProducts
 using DistributedFactorGraphs
 using LieGroups
+using JSON
 
+import DistributedFactorGraphs: DFGJSONStyle, TypeMetadata, Packed, resolvePackedType
+import StructUtils: StructUtils, structlike, lower, lift, make
+import ApproxManifoldProducts: HomotopyRepr
+
+# JSON.json("$varFolder/$(v.label).json", v; style = DFGJSONStyle())
 
 @defStateType(
   TestTranslation2, 
@@ -13,6 +19,59 @@ using LieGroups
 )
 
 ##
+
+hr = HomotopyRepr{
+  MajorMaxDepth{3},
+  ConcentratedGaussianKernel, 
+  TestTranslation2, 
+  Nothing, 
+}(TestTranslation2(), nothing)
+
+
+##
+
+
+# # StructUtils.structlike(::DFGJSONStyle, ::Type{<:HomotopyRepr}) = false
+# # StructUtils.arraylike(::DFGJSONStyle, ::Type{<:HomotopyRepr}) = false
+# # StructUtils.lower(::DFGJSONStyle, x::HomotopyRepr) = string(typeof(x))
+# # function StructUtils.lift(::DFGJSONStyle, ::Type{<:HomotopyRepr}, x::AbstractString)
+# #     return getfield(Main, :HomotopyRepr), nothing
+# # end
+
+# StructUtils.structlike(::Type{<:HomotopyRepr}) = false
+# StructUtils.lower(T::HomotopyRepr) = lowerHomotopyRepr(T)
+# StructUtils.lift(::Type{HomotopyRepr}, s) = liftHomotopyRepr(s)
+
+
+# function lowerHomotopyRepr(varT::H) where {A,B,C,D, H<:HomotopyRepr{A,B,C,D}}
+#   typemeta = TypeMetadata(typeof(varT))
+#   partial = JSON.json(string(varT.partial); style = DFGJSONStyle())
+#   return TypeMetadata(
+#     typemeta.pkg,
+#     Symbol(typemeta.name, "{", A,", ", B, ", ", C, ", ", partial, "}"),
+#     typemeta.version,
+#   )
+# end
+
+
+# function liftHomotopyRepr(type::DFG.JSON.Object)
+#   @info type
+#   error("FIXME liftHomotopyRepr not implemented yet")
+# end
+
+##
+
+
+JSON.json("/tmp/hr.json", Packed(hr); style = DistributedFactorGraphs.DFGJSONStyle())
+
+##
+
+
+hr3 = JSON.parse("/tmp/hr.json"; style = DistributedFactorGraphs.DFGJSONStyle())
+
+
+##
+
 
 function demo_lowerpack(
   hode::HomotopyDensity
@@ -80,3 +139,65 @@ rootkernel = getKernelTree(hode, 1)
 
 ##
 
+
+
+
+
+## DEV STUFF
+
+
+
+using Test
+using ApproxManifoldProducts
+using DistributedFactorGraphs
+using LieGroups
+using JSON
+
+import DistributedFactorGraphs: DFGJSONStyle, TypeMetadata, Packed, resolvePackedType
+import StructUtils: StructUtils, structlike, lower, lift, make
+import ApproxManifoldProducts: HomotopyRepr
+
+# JSON.json("$varFolder/$(v.label).json", v; style = DFGJSONStyle())
+
+@defStateType(
+  TestTranslation2, 
+  TranslationGroup(2), 
+  zeros(2)
+)
+
+##
+
+hr = HomotopyRepr{
+  MajorMaxDepth{3},
+  ConcentratedGaussianKernel, 
+  TestTranslation2, 
+  Nothing, 
+}(TestTranslation2(), nothing)
+
+
+struct MyType{T}
+  t::Type{T}
+end
+
+function StructUtils.make(::Type{<:MyType}, x)
+  error("THIS")
+end
+function liftMyType(type::JSON.Object)
+  error("THAT")
+end
+StructUtils.lift(::DFGJSONStyle, ::Type{<:MyType}, s) = liftMyType(s)
+
+
+StructUtils.structlike(::Type{<:MyType}) = false
+
+##
+
+a = MyType(Int)
+
+jstr = JSON.json(Packed(a); style = DFGJSONStyle())
+
+uo = JSON.parse(jstr; style = DFGJSONStyle())
+
+# expecting uo to return the equivalent of MyType{Int}(t=Int) --- resolvePackedType only does type?
+
+##
