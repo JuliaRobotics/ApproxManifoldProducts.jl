65d83da24de7e126ab8a23230897d2fa3d6a5b82
dehann <dehann.fourie@gmail.com>
2026-07-10 18:18:47 -0700

    more MargProd tests pass

--- test/testMarginalProducts.jl
+++ test/testMarginalProducts.jl
@@ -786,8 +786,8 @@ end
         N_ = length(pts)
         @cast pGM[i, j] := pts[j][i]
 
-        @test_broken 0.7 * N_ < sum(-13 .< pGM[1, :] .< -7)
-        @test_broken 0.7 * N_ < sum(7 .< pGM[3, :] .< 13)
+        @test 0.7 * N_ < sum(-13 .< pGM[1, :] .< -7)
+        @test 0.7 * N_ < sum(7 .< pGM[3, :] .< 13)
 
     ## check the selection of labels and resulting Gaussian products are correct
 
